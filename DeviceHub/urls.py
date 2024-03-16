ccb01390948ce2555d6b4bb68459e14bcdabf3a4
nasif raihan <nasif.raihan78@gmail.com>
2024-03-16 22:31:22 +0600

    initiated the project

---
+++ DeviceHub/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+urlpatterns = [path("admin/", admin.site.urls), path("", include(router.urls))]
